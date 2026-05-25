bde28f4ed47abbfbceb58332d6c551ec3db0b875
Цветанка Йосифова <tsnaneva@gmail.com>
2026-05-25 13:54:47 +0300

    Create 06.TheatrePromotions.js

---
+++ 01_Lab_JS Basic Syntax/06.TheatrePromotions.js
@@ -0,0 +1,48 @@
+function theatrePromotions(day, age){
+    let price = 0;
+
+    if (age < 0 || age > 122) {
+    console.log("Error!");
+    return;
+    }
+
+    switch(day){
+        case 'Weekday': 
+            if (age >= 0 && age <= 18){
+                price = 12;
+            } else if (age > 18 && age <= 64){
+                price = 18;
+            } else if (age > 64 && age <= 122){
+                price = 12;
+            }
+            break;
+        case 'Weekend': 
+            if (age >= 0 && age <= 18){
+                price = 15;
+            } else if (age > 18 && age <= 64){
+                price = 20;
+            } else if (age > 64 && age <= 122){
+                price = 15;
+            }
+            break;
+        case 'Holiday': 
+            if (age >= 0 && age <= 18){
+                price = 5;
+            } else if (age > 18 && age <= 64){
+                price = 12;
+            } else if (age > 64 && age <= 122){
+                price = 10;
+            }
+            break;
+        default:
+            console.log(`Error!`);
+            break;
+    }
+
+    console.log(`${price}$`);
+    
+}
+
+theatrePromotions('Weekday', 42);
+theatrePromotions('Holiday', -12);
+theatrePromotions('Holiday', 15);
