9c5e2a8e7789c9f4f52ffaf5e8fe8c9481cba600
Цветанка Йосифова <tsnaneva@gmail.com>
2026-05-27 09:47:41 +0300

    Create 07.TriangleOfNumbers.js

---
+++ 01_EX_JS Basik Syntax/07.TriangleOfNumbers.js
@@ -0,0 +1,17 @@
+function triangleOfNumbers (endNum) {
+   
+    for (i = 1; i <= endNum; i++) {
+    let nubers = ''; 
+
+        for (j = 1; j<= i; j++){
+           
+            nubers += `${i} `
+            
+        }       
+
+        console.log(nubers);
+    }
+            
+}
+
+triangleOfNumbers(6);
