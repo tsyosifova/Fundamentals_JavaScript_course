56d4c9746b4429cd1a7a0b01961966e02b824feb
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-07 11:58:00 +0300

    Create 07.NxNMatrix.js

---
+++ 04_EX_Functions/07.NxNMatrix.js
@@ -0,0 +1,17 @@
+function printNxNMatrix(num) {
+
+    for (let i = 0; i < num; i++) {
+        let row = '';
+
+        for (let j = 0; j < num; j++) {
+            
+            row += num + ' ';
+
+        }
+
+        console.log(row);
+    }
+
+}
+
+printNxNMatrix(3);
