c9986681d5c177b93f4db69db1ecdd297be01774
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-11 10:55:11 +0300

    Create 04.LastKNumbersSequence.js

---
+++ 05_Lab_Arrays_Advanced.js/04.LastKNumbersSequence.js
@@ -0,0 +1,27 @@
+function lastKNumbersSequence(n, k) {
+
+    let result = [1];
+
+
+    //console.log(result);
+
+    for(let i = 1; i < n; i++) {
+            let startEl = Math.max(0,i - k);
+
+            let prevThreeEl = result.slice(startEl, i);
+
+            let sum = 0;
+
+            for (let number of prevThreeEl) {
+                sum += number; 
+            }
+
+            result.push(sum);
+
+    }
+
+    console.log(result.join(' '));
+}
+
+lastKNumbersSequence(6, 3);
+lastKNumbersSequence(8, 2);
