349a7afc3f8b12f81ef0e03815ef5cab6aa63659
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-12 08:49:43 +0300

    practive

---
+++ 05_Lab_Arrays_Advanced.js/05.ProcessOddNumbers.js
@@ -0,0 +1,9 @@
+function processOddNumbers(arr) {
+    let result = arr
+        .filter((el, i) => i % 2 === 1)
+        .map(el => el * 2)
+        .reverse();
+
+
+    console.log(result.join(' '));
+}
