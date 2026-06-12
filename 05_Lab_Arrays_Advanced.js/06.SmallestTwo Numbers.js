54661c87b636cc93572d83bd52e49d0b36e1b4ba
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-12 13:06:50 +0300

    Create 06.SmallestTwo Numbers.js

---
+++ 05_Lab_Arrays_Advanced.js/06.SmallestTwo Numbers.js
@@ -0,0 +1,10 @@
+function smallestTwoNumbers(arr) {
+    let sorted = arr.sort((a, b) => a - b);
+    let twoSmalest = sorted.slice(0, 2);
+
+    console.log(twoSmalest.join(' '));
+
+}
+
+smallestTwoNumbers([30, 15, 50, 5]);
+smallestTwoNumbers([3, 0, 10, 4, 7, 3]);
