4682142491d85d9f8eabf06d56ee359ae67af7d8
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-16 07:41:35 +0300

    Create 08.Calculator.js

---
+++ 02_EX_Date Types and Var/08.Calculator.js
@@ -0,0 +1,19 @@
+function calculator(num1, operator, num2) {
+    switch (operator) {
+       case '+':
+            console.log((num1 + num2).toFixed(2));
+            break;
+       case '-':
+            console.log((num1 - num2).toFixed(2));
+            break;
+       case '*':
+            console.log((num1 * num2).toFixed(2));
+            break;
+       case '/':
+            console.log((num1 / num2).toFixed(2));
+            break;
+    }
+    
+}
+
+calculator(5, '+', 10);
