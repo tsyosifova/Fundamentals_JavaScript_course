c6a0ec94a3dacc9a7a2424463414f33e91775047
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-23 21:05:18 +0300

    practice

---
+++ 07_Lab_Objects-and-Classes/01.PersonInfo.js
@@ -0,0 +1,21 @@
+function personInfo(firstName, lastName, age) {
+
+    let person = {
+        firstName,
+        lastName,
+        age
+    };
+
+    //console.log(person);
+    return person;
+}
+
+personInfo("Peter",
+    "Pan",
+    "20"
+);
+
+personInfo("George",
+    "Smith",
+    "18"
+);
