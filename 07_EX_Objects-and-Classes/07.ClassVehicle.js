c2938b32e2dceb1e13ff11202e48a683e500ce32
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-27 16:59:41 +0300

    practice

---
+++ 07_EX_Objects-and-Classes/07.ClassVehicle.js
@@ -0,0 +1,20 @@
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        parts.quality = parts.engine * parts.power;
+
+        this.type = type;
+        this.model = model;
+        this.parts = parts;
+        this.fuel = fuel;
+    }
+
+    drive(fuelLoss) {
+        this.fuel -= fuelLoss;
+    } 
+}
+
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
