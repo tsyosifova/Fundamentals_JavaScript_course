4ee34fce45e4bf58e8b02ad150b13737ed1cddfa
Цветанка Йосифова <tsnaneva@gmail.com>
2026-06-29 17:20:44 +0300

    practice

---
+++ 07_EX_Objects-and-Classes/00.testObj.js
@@ -0,0 +1,7 @@
+let obj = {};
+
+obj["Beer"] = 2;
+obj["Pasta"] = 11;
+obj["Bananas"] = 44;
+
+console.log(obj);
--- 07_EX_Objects-and-Classes/03.StoreProvision.js
+++ 07_EX_Objects-and-Classes/03.StoreProvision.js
@@ -1,40 +1,36 @@
 function storeProvision(currProductsArr, orderedProductsArr) {
-    let productObg = {};
+    let productObj = {};
+    let product = '';
+    let quntity = 0;
 
     for (let i = 0; i < currProductsArr.length; i += 2) {
+
         product = currProductsArr[i];
         quntity = Number(currProductsArr[i + 1]);
 
-        if (orderedProductsArr.includes(product)) {
-            let indPr = orderedProductsArr.indexOf(product);
-            quntityOrderd = Number(orderedProductsArr[indPr + 1]);
-            quntity += quntityOrderd;
-            orderedProductsArr.splice(indPr, 2);
-
-            for (let j = 0; j < orderedProductsArr.length; j += 2) {
-                let productOrd = orderedProductsArr[j];
-            }
-        }
-
-        productObg = {
-            prod: product,
-            qunt: quntity
-        };
-
-        console.log(`${productObg.prod} -> ${productObg.qunt}`);
+        productObj[product] = quntity;
 
     }
 
     for (let i = 0; i < orderedProductsArr.length; i += 2) {
-        let product = orderedProductsArr[i];
-        let quntity = orderedProductsArr[i + 1];
 
-        productObg = {
-            prod: product,
-            qunt: quntity
-        };
+        product = orderedProductsArr[i];
+        quntity = Number(orderedProductsArr[i + 1]);
 
-        console.log(`${productObg.prod} -> ${productObg.qunt}`);
+        if (productObj[product] === undefined) {
+            productObj[product] = quntity;
+        } else {
+            productObj[product] += quntity;
+        }
+
+    }
+
+    let entries = Object.entries(productObj);//Масив от свойствата като масиви 
+
+    for (let entry of entries) {
+        let [key, value] = entry;//деструктурирам масива на всяко свойство на ключ и стойност
+        //console.log(entry);
+        console.log(`${key} -> ${value}`);//печатам чрез параметрите в искания формат
     }
 }
 
