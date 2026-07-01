89436839bcaa20ece891440d577dffe2441b7794
Цветанка Йосифова <tsnaneva@gmail.com>
2026-07-01 09:05:21 +0300

    Create 09.Catalogue.js

---
+++ 07_EX_Objects-and-Classes/09.Catalogue.js
@@ -0,0 +1,14 @@
+function catalogue(arr) {
+console.log('Предстои решение!')м;
+}
+
+catalogue([
+    'Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10'
+]);
