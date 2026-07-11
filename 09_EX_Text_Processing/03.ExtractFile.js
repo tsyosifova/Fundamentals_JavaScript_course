c575d9c0a825eb87ff35a2ff223f84580f7b24f1
Цветанка Йосифова <tsnaneva@gmail.com>
2026-07-11 11:08:26 +0300

    Create 03.ExtractFile.js

---
+++ 09_EX_Text_Processing/03.ExtractFile.js
@@ -0,0 +1,13 @@
+function solve(filePath) {
+    let filePathArr = filePath.split('\\');
+    let file = filePathArr.pop();
+    let dotInd = file.lastIndexOf('.');
+    let faleName = file.substring(0, dotInd);
+    let fleExtension = file.substring(dotInd + 1);
+
+    console.log(faleName);
+    console.log(fleExtension);
+    
+}
+
+solve('C:\\Internal\\training-internal\\Template.pptx');
