6cd917ee06beb4248d0eae2f0369e0f723d24f33
Цветанка Йосифова <tsnaneva@gmail.com>
2026-07-21 09:53:45 +0300

    Update 5.NetherRealms.js

--- 10_EX_Reg_Exp/5.NetherRealms.js
+++ 10_EX_Reg_Exp/5.NetherRealms.js
@@ -1,26 +1,57 @@
 function solve(string) {
-    let demonsArr = string.split(/\s*,\s/).sort((a, b) => a.localeCompare(b));
+    let demonsArr = string.split(/\s*,\s*/).sort((a, b) => a.localeCompare(b));
     let demonsHealthDamage = {};
     let healthPattern = /[^0-9+\-*\/.]/g;
-    //let damagePattern = //;
-    let health = 0;
-    let damage = 0;
-
+    let damagePattern = /[-+]?\d+(\.\d+)?/g;
+    let operatorPattern = /[*\/]/g;
+    
     for (let demon of demonsArr) {
 
-        let healthSymbols = demon.match(healthPattern);
+        let health = 0;
+        let damage = 0;
 
-        for (let symbol of healthSymbols) {
-            let ascii = symbol.charCodeAt();
-            health += ascii; 
+        let healthSymbolsMatches = demon.match(healthPattern);
+        let demonDigitsMatches = demon.match(damagePattern);
+
+        if (healthSymbolsMatches) {
+
+            for (let symbol of healthSymbolsMatches) {
+                let ascii = symbol.charCodeAt();
+                health += ascii;
+            }
         }
 
-        console.log(health);
+        if (demonDigitsMatches) {
+            for (let digit of demonDigitsMatches) {
+                damage += Number(digit);
+            }
+        }
+
+        let operatorMatches = demon.match(operatorPattern);
+
+        if (operatorMatches) {
+            for (let operator of operatorMatches) {
+                
+                if(operator === '*') {
+                    damage *= 2;
+
+                } else {
+                    damage /= 2;
+                }
+            }
+        }
+
+        demonsHealthDamage[demon] = {health, damage};
+
     }
+   
+    let entries = Object.entries(demonsHealthDamage);
 
+    for (let [name, infoObj] of entries) {
 
-   //console.log(health);
-
+        console.log(`${name} - ${infoObj.health} health, ${infoObj.damage.toFixed(2)} damage`);
+    }
+    
 }
 
 solve('M3ph1st0**, Azazel')
