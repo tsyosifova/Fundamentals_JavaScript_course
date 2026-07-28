b2197c5c4dbd52a2f9382e31017acb8ded39c557
Цветанка Йосифова <tsnaneva@gmail.com>
2026-07-28 16:08:43 +0300

    Update 03.js

--- 10_EX_Reg_Exp/03.js
+++ 10_EX_Reg_Exp/03.js
@@ -2,19 +2,19 @@ function solve(arr) {
     let input = arr.shift();
     let totalPrice = 0;
     let income = 0;
-    let pattern = /%(?<name>[A-Z][a-z]+)%[^|\$%\.]*\<(?<product>\w+)\>[^|\$%\.]*\|(?<count>\d+)\|[^|\$%]*(?<price>\d+(?:\.\d+)?)\$/;
+    let pattern = /%(?<name>[A-Z][a-z]+)%[^|\$%\.]*\<(?<product>\w+)\>[^|\$%\.]*\|(?<count>\d+)\|[^|\$%\.]*?(?<price>\d+(\.\d+)?)\$/;
 
     while (input !== `end of shift`) {
         let match = pattern.exec(input);
 
         if (match) {
-        let customerName = match.groups.name;
-        let productName = match.groups.product;
-        let totalPrice = Number(match.groups.count) * Number(match.groups.price);
-        
-        income += totalPrice;
+            let customerName = match.groups.name;
+            let productName = match.groups.product;
+            let totalPrice = Number(match.groups.count) * Number(match.groups.price);
 
-        console.log(`${customerName}: ${productName} - ${totalPrice.toFixed(2)}`);
+            income += totalPrice;
+
+            console.log(`${customerName}: ${productName} - ${totalPrice.toFixed(2)}`);
         }
 
         input = arr.shift();
@@ -27,4 +27,10 @@ function solve(arr) {
 solve(['%George%<Croissant>|2|10.3$',
     '%Peter%<Gum>|1|1.3$',
     '%Maria%<Cola>|1|2.4$',
+    'end of shift']);
+
+solve(['%InvalidName%<Croissant>|2|10.3$',
+    '%Peter%<Gum>1.3$',
+    '%Maria%<Cola>|1|2.4',
+    '%Valid%<Valid>valid|10|valid20$',
     'end of shift']);
